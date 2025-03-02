1869d3bcc4c7169a113ff7780253b8cedd439cb6
deserteagle-noob <alokkumardeserteagle@gmail.com>
2025-03-02 08:54:46 +0530

    second commit

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "../public/index.html"));
 });
 
 app.get("/pincode/:pincode", async (req, res) => {
